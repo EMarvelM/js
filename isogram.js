ee3c5809db6c4e91d032e4b3963af55dc4b99d59
EMarvelM <marvelousegbe19@gmail.com>
2024-03-15 07:03:04 +0100

    implement the isogram fnc to determine is letter repeat in a string

--- isogram.js
+++ isogram.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-function isIsogram(str){
-  //...
-  str.forEach((element) => )
 
+function isIsogram(str){
+    //...
+    let coun = 0;
+    stri = str.split('');
+    stri.forEach(
+        ele => { coun += stri.filter(element => element === ele).length }
+    )
+    if (coun > str.length) {return false} else { return false }
 }
+
+console.log(isIsogram("ada"));
